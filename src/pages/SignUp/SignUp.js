c679812249c7f5ae66c7a14aa3abce0cf3e5e03a
mareadelmar <mar.ortega.v@gmail.com>
2021-09-24 19:42:07 -0300

    Fixing signup's button styles

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -2,6 +2,13 @@ import React, { useState, useEffect } from "react";
 import { useLocation } from "wouter";
 import useUserData from "../../hooks/useUserData";
 import { Helmet } from "react-helmet";
+import { makeStyles, Button } from "@material-ui/core";
+
+const useStyle = makeStyles({
+    btnCustom: {
+        marginTop: "1em",
+    },
+});
 
 const SignUp = () => {
     const title = "Freaks | Login";
@@ -9,6 +16,7 @@ const SignUp = () => {
     const [password, setPassword] = useState("");
     const { userLogged, signUp } = useUserData();
     const [, pushLocation] = useLocation();
+    const classes = useStyle();
 
     const handleSignUp = (e) => {
         e.preventDefault();
@@ -43,13 +51,22 @@ const SignUp = () => {
                         onChange={(e) => setPassword(e.target.value)}
                         type="password"
                     />
-                    <input
+                    {/* <input
                         className="form-btn-login"
                         id="btn-signup"
                         onClick={handleSignUp}
                         type="submit"
                         value="Sign up"
-                    />
+                    /> */}
+                    <Button
+                        onClick={handleSignUp}
+                        className={classes.btnCustom}
+                        color="primary"
+                        variant="contained"
+                        fullWidth
+                    >
+                        Sign up
+                    </Button>
                 </form>
             </div>
         </>
