ce1e727d3495d941053834579322f49b04996bfc
mareadelmar <mar.ortega.v@gmail.com>
2021-10-02 18:34:21 -0300

    unifying watchlist's service functions

--- src/hooks/useWatchlist.js
+++ src/hooks/useWatchlist.js
@@ -1,20 +1,27 @@
 import { useState, useContext, useEffect, useCallback } from "react";
 import UserContext from "../context/UserContext";
 import useUserData from "./useUserData";
-import getWatchlist from "../services/getWatchlistService";
-import setWatchlist from "../services/setWatchlistService";
+import { postWatchlist, getWatchlist, deleteWatchlist } from "../services/watchlistService";
 
 export default function useWatchlist(){
     const { userWatchlist, setUserWatchlist } = useContext(UserContext);
     const { userLogged, userData } = useUserData();
     const [ loading, setLoading ] = useState(false);
+    const [ errorMsn, setErrorMsn ] = useState("")
 
     const userId = userData ? userData.uid : null;
 
     const addToWatchlist = useCallback((movie) => {
-        setWatchlist(userId, movie)
-            .then(res => console.log(res))
-            .catch(err => console.log(err));
+        postWatchlist(userId, movie)
+            .then(() => {
+                console.log("subido!!")
+                setLoading(false);
+            })
+            .catch(err => {
+                console.error(err)
+                setErrorMsn(err.message);
+                setLoading(false);
+            })
     }, [userId])
 
     useEffect(() => {
@@ -30,6 +37,7 @@ export default function useWatchlist(){
     return {
         userWatchlist,
         loading,
+        errorMsn,
         addToWatchlist
     }
 }
---
+++ src/services/watchlistService.js
@@ -0,0 +1,35 @@
+import { db } from "../config/firebaseConfig";
+
+export function postWatchlist(userId, movie){
+    const WLRef = db.collection("watchlist");
+    return WLRef
+            .doc(userId)
+            .collection("userWatchlist")
+            .add(movie);
+}
+
+export const deleteWatchlist = (uid, movieId) => {
+    const WLRef = db.collection("watchlist");
+    return WLRef
+            .doc(uid)
+            .collection("userFavs")
+            .doc(String(movieId))
+            .delete()
+}
+
+export function getWathclist(userId){
+    const WLRef = db.collection("watchlist").doc(userId).collection("userWatchlist");
+
+    return WLRef.get().then((doc) => {
+        console.log("siii")
+        if (doc) {
+            let watchArray = [];
+            doc.forEach((item) => {
+                watchArray.push(item.data());
+            });
+            return watchArray;
+        } else {
+            console.log("not found");
+        }
+    })
+}
